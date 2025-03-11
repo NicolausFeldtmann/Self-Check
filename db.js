32e5601cd3d8ffdc08d0e54c7c78757c0580e158
NicolausFeldtmann <n.feldtmann@yahoo.de>
2025-03-11 09:21:23 +0100

    first commit

---
+++ db.js
@@ -0,0 +1,71 @@
+let data = [
+    {
+        "headline": "Selbstwert und Gelassenheit",
+        "question": "Kann ich auf Anheib 5 positive Eigenschaften von mir aufzählen?",
+        "answer1": "Ja, absolut!",
+        "answer2": "Teilweise, ich muss schon ein wenig nachdenken.",
+        "answer3": "Nein, so sehr ich auch suche, ich find nichts."
+    },
+    {
+        "headline": "Selbstwert und Gelassemheit",
+        "question": "Komm ich gut damit zurecht, wenn ich meine Aufgaben nicht komolett erleige?",
+        "answer1": "Ja, es fällt mir nicht schwer los zu lassen;",
+        "answer2": "Teils, ich muss schon sehr bewust los lassen.",
+        "answer3": "Garnicht, nicht erledigtes verfolgt mich noch lanfge",
+    },
+    {
+        "headline": "Selbstwert und Gelassenheit",
+        "question": "Ich habe meine Schwächen, aber die gehören zu mir und das ist ok.",
+        "answer1": "Natürlich! Niemand ist perfekt und meine Fehler machen mich menschlich.",
+        "answer2": "Zum Teil. Ich Fehler sind zwar menschlich, aber meine fallen mir schon sehr auf.",
+        "answer3": "Eher nicht. Ich muss mir meine Fehler vorhalten!",
+    },
+    {
+        "headline": "Selbstwert und Gelassenheit",
+        "question": "Mein Körper ist nicht perfekt, aber es ist meiner und ich mag ihn?",
+        "answer1": "Ja, mir fallen soagar Ding an mir auf, die mir gefallen.",
+        "answer2": "Teils, teils. Ich bin zwar nicht zufreiden, aber ich fühle mich grundsätlich wohl",
+        "asnwer3": "Nein! Ich mag meinen Körper nicht und schäme mich für ihn.",
+    },
+    {
+        "headline": "Ich vertraue meiner Intuition un vertraue meinem Urteilsvermögen.",
+        "answer1": "Absolut! Ich mein Bauchgefühl hat mich schon oft sicher geleitet.",
+        "answer2": "Zeitweise. Manchmal treffe ich instinktiv gute Entscheidungen, manchmal aber auch nicht.",
+        "anser3": "Auf keinen Fall! Ich entscheide mich immer falsch!",
+    },
+    {
+        "headline": "Offenheit und positive Gedanken",
+        "question": "Wenn ich Morgends aufstehe, freu ich mich auf den Tag!",
+        "answer1": "Ja! Jeder neue Tag bring neues Glück!",
+        "answer2": "Manchmal, manchmal aber auch nicht.",
+        "answer3": "Niemals! Ich wache auf und fühle mich miserabel.",
+    },
+    {
+        "headline": "Offenheit und positives Denken",
+        "question": "Ich denke alles was ih für mein Glüchk brauche, habe ich in mir.",
+        "answer1": "Selbstverständlich ist jeder seinen Glückes Schmied!",
+        "anser2": "Nur zum Teil. Ich hab zwar Einfluss drauf, aber nicht moplett Kontrolle darüber.",
+        "answer3": "Eher nivht, ivh bin nicht glücklich und kann nichts dagegen tun.",
+    },
+    {
+        "headline": "Offenheit und positives Denken",
+        "question": "Ich kenne meine Stärken und kann mein Leben damit zum guten wernden.",
+        "answer1": "Ich weis was ich kann und wie ich mein Leben leben will",
+        "answer2": "Teilweise, Ich kenne meine Stärken aber eben auch meine Grenzen.",
+        "answer3": "Oh nein, ich kann nicht viel und bin machtlos.",
+    },
+    {
+        "headline": "Offenheit und positives Denken",
+        "question": "Gehe ich mit Niederlagen gelassen um?",
+        "answer1": "Scheitern gehört zum Leben und vieleivht kann ich sogar was lernen.",
+        "answer2": "Wenn ich scheitere, dann bellastert mich da schon ein wenig.",
+        "answeer3": "Mit Rüchschlägen kann nich nicht umgehen und ich mache mir Vorwürfe deswegen.",
+    },
+    {
+        "headline": "Offenheit und positives Denken",
+        "question": "Ich bin offee für neues und ungewohntes",
+        "answer1": "Neues zu lernen ist immer etwas schöness",
+        "answer2": "Ich hab schon ein wenig meine Mühe nicht voreingenommen zu sein",
+        "answer3": "Eher unwascheinlich! Fremdes ist mir eher unbehaglich." 
+    },
+]
